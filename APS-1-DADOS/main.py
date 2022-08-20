5ef9436ebf32f841e020f79a5e15ee3bd4865091
Fabio Marturano <fabiocmjr@gmail.com>
2022-08-20 13:04:13 -0300

    ajuste no json e analise dos dados

--- APS-1-DADOS/main.py
+++ APS-1-DADOS/main.py
@@ -3,69 +3,54 @@ import pandas as pd
 import json
 
 
-def program():
-    # name = input("nome ? >>>")
-    # print("seu nome" + name)
-
-    matriz = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]).reshape(3, 3)
-    print(matriz)
-    print("\n")
-
-    soma = np.sum(matriz[:, 0])  # parametro da matriz [todas as linhas : coluna 0]
-    print(soma)
-    print("\n")
-
-    idades = np.array([12, 23, 32, 45, 56, 63, 75, 82, 91]).reshape(3, 3)
-    mediaIdades = np.mean(idades[:, :])
-    print(mediaIdades)
-    print("\n")
-
-    if mediaIdades <= 18:
-        print("Pessoas novas")
-    elif 18 < mediaIdades < 40:
-        print("Pessoas Maduras")
-    else:
-        print("Pessoas Velhas")
-
-    sequencial = np.arange(10)
-    print(sequencial)
-    print("\n")
-
-    matrizSequencial = np.arange(9).reshape(3, 3)
-    print(matrizSequencial)
-    print("\n")
-
-    matrizUm = np.ones((3, 3))
-    matrizZero = np.zeros((3, 3))
-
-    print(matrizUm)
-    print(matrizZero)
-    print("\n")
-
+def importaArquivo(nomeArquivo):
     try:
-        file = np.genfromtxt('arquivo.txt', delimiter=',')
-        print('arquivo carregado')
-        print(file)
-        print(np.sum(file[:, :]))
-    except:
-        print('arquivo não carregado')
-
-    print("\n")
-
-    try:
-        loadFile = open("arquivoJson.txt", "r")
+        loadFile = open(nomeArquivo, "r")
         read = loadFile.read()
         jsonData = json.loads(read)
         print('arquivo carregado')
         print(jsonData)
+        print('\n')
+        return jsonData
     except:
         print('arquivo não carregado')
 
-    # importa json
-    # abrir
-    # analisar (soma, media, minimo, maximo ou contagem) 
-    # analisar (if, else)
-    # tirar conclusão
+
+def analizaNotasAlunos(notas):
+    print('########## Notas ##########')
+    print(f'Total: {np.sum(notas)}')
+    print(f'Media: {np.mean(notas)}')
+    print(f'Desvio Padrão: {np.sum(notas)}')
+    print(f'Minimas: {np.min(notas)}')
+    print(f'Maxima: {np.max(notas)}')
+    print('\n')
+
+
+def analizaSituacao(situacoes):
+    aprovado = 0
+    reprovado = 0
+    trancado = 0
+
+    for situacao in situacoes:
+        if situacao == "aprovado":
+            aprovado += 1
+        if situacao == "reprovado":
+            reprovado += 1
+        if situacao == "trancado":
+            trancado += 1
+
+    print('########## Situações ##########')
+    print(f'Aprovados {aprovado}')
+    print(f'Reprovados {reprovado}')
+    print(f'Trancados {trancado}')
+
+
+def program():
+    alunos = importaArquivo("arquivoJson.txt")["alunos"]
+
+    analizaNotasAlunos(np.array([aluno['notaFinal'] for aluno in alunos]))
+    analizaSituacao(np.array([aluno['situacao'] for aluno in alunos]))
+
 
 if __name__ == '__main__':
     program()
